cd9cc78950519341523d1fc18a225e7fd265de5b
RusoDan8 <141376625+RusoDan8@users.noreply.github.com>
2024-08-30 20:23:32 -0600

    Add files via upload

---
+++ JavaAPI/ChilaquilesRestaurantApiApplication.java
@@ -0,0 +1,10 @@
+package com.chilaquilesrestaurant.principal;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilaquilesRestaurantApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChilaquilesRestaurantApiApplication.class, args);
+    }
+}
